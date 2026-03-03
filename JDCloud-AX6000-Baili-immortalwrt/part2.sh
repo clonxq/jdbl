6ec909eb2520472fb3b8781e8a0a6ea7163d11bc
xqrock <58764635+xqrock@users.noreply.github.com>
2026-03-03 20:41:46 +0800

    Create part2.sh

---
+++ JDCloud-AX6000-Baili-immortalwrt/part2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+#修改默认IP地址
+sed -i 's/192\.168\.[0-9]*\.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+##-----------------Del duplicate packages------------------
+rm -rf feeds/packages/net/open-app-filter
+rm -rf package/feeds/luci/luci-app-wrtbwmon
+rm -rf package/feeds/packages/wrtbwmon
+rm -rf ./feeds/packages/net/adguardhome
+rm -rf ./feeds/packages/net/mosdns
+# rm -rf ./feeds/packages/net/shadowsocks-libev
+# rm -rf ./feeds/packages/net/shadowsocks-rust
+# rm -rf ./feeds/packages/net/shadowsocksr-libev
+rm -rf ./feeds/luci/applications/luci-app-ssr-plus
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+#修复Rust编译失败
+sed -i 's/ci-llvm=true/ci-llvm=false/g' feeds/packages/lang/rust/Makefile
+# 修复 mbedtls 在 GCC 14 下的 target specific option mismatch 错误
+find ./package/libs/mbedtls -name Makefile -exec sed -i 's/-Werror//g' {} +
+find ./feeds/base/package/libs/mbedtls -name Makefile -exec sed -i 's/-Werror//g' {} +
