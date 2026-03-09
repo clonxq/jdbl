c8048a62b780cfc9f8091ea41ce8d54914bc504f
xqrock <58764635+xqrock@users.noreply.github.com>
2026-03-09 08:40:17 +0800

    Update diy-part2.sh

--- JDCloud-AX6000-Baili-immortalwrt/diy-part2.sh
+++ JDCloud-AX6000-Baili-immortalwrt/diy-part2.sh
@@ -13,17 +13,17 @@
 sed -i 's/192\.168\.[0-9]*\.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 ##-----------------Del duplicate packages------------------
-rm -rf feeds/packages/net/open-app-filter
-rm -rf package/feeds/luci/luci-app-wrtbwmon
-rm -rf package/feeds/luci/luci-app-wechatpush
-rm -rf package/feeds/luci/luci-app-openclash
-rm -rf package/feeds/packages/wrtbwmon
-rm -rf ./feeds/packages/net/adguardhome
-rm -rf ./feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/open-app-filter
+rm -rf feeds/luci/applications/luci-app-wrtbwmon
+rm -rf feeds/luci/applications/luci-app-wechatpush
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/packages/net/wrtbwmon
+rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/packages/net/mosdns
 # rm -rf ./feeds/packages/net/shadowsocks-libev
 # rm -rf ./feeds/packages/net/shadowsocks-rust
 # rm -rf ./feeds/packages/net/shadowsocksr-libev
-rm -rf ./feeds/luci/applications/luci-app-ssr-plus
+rm -rf feeds/luci/applications/luci-app-ssr-plus
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/packages/lang/golang
