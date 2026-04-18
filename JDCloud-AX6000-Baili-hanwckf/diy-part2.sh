26c80931bbfe191cb57bbc9ec70614c68d8bb338
xqrock <58764635+xqrock@users.noreply.github.com>
2026-04-19 01:06:02 +0800

    Update diy-part2.sh

--- JDCloud-AX6000-Baili-hanwckf/diy-part2.sh
+++ JDCloud-AX6000-Baili-hanwckf/diy-part2.sh
@@ -11,11 +11,13 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.51.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192\.168\.[0-9]*\.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 ##-----------------Del duplicate packages------------------
 rm -rf feeds/packages/net/open-app-filter
 rm -rf package/diy/passwall-packages/shadowsocks*
 rm -rf feeds/packages/net/shadowsocks*
+rm -rf ./feeds/packages/net/adguardhome
+rm -rf ./feeds/packages/net/mosdns
 rm -rf feeds/luci/applications/luci-app-ssr-plus
 rm -rf feeds/luci/applications/luci-app-wechatpush
 rm -rf feeds/luci/applications/luci-app-vssr
@@ -23,18 +25,6 @@ rm -rf feeds/luci/applications/luci-app-ddns-go
 ##-----------------Manually set CPU frequency for MT7986A-----------------
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 rm -rf feeds/luci/applications/luci-app-passwall
-
-# 自动化清理冲突包 (核心完善版)
-function auto_remove_conflicts() {
-    local diy_dir="package/diy"
-    [ ! -d "$diy_dir" ] && return
-    echo ">>> 开始自动扫描 package/diy 并清理 feeds 冲突..."
-    for pkg in $(find "$diy_dir" -maxdepth 1 -mindepth 1 -type d -exec basename {} \;); do
-        # 在 feeds 全目录搜索同名包并删除
-        find feeds/ -type d -name "$pkg" | xargs -r rm -rf
-    done
-}
-auto_remove_conflicts
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
