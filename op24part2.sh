2b305564648f7d0d34377a4c0a8face367443a27
xqrock <58764635+xqrock@users.noreply.github.com>
2026-09-16 14:28:54 +0800

    Create op24part2.sh

---
+++ op24part2.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+#修改默认IP地址
+sed -i 's/192\.168\.[0-9]*\.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+##-----------------Del duplicate packages------------------
+rm -rf feeds/luci/applications/open-app-filter
+rm -rf feeds/luci/applications/luci-app-wrtbwmon
+rm -rf feeds/luci/applications/luci-app-wechatpush
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-ssr-plus
+rm -rf feeds/packages/net/wrtbwmon
+rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/packages/net/mosdns
+# rm -rf ./feeds/packages/net/shadowsocks-libev
+# rm -rf ./feeds/packages/net/shadowsocks-rust
+# rm -rf ./feeds/packages/net/shadowsocksr-libev
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 27.x feeds/packages/lang/golang
+#修复Rust编译失败
+sed -i 's/ci-llvm=true/ci-llvm=false/g' feeds/packages/lang/rust/Makefile
+sed -i 's|../../luci.mk|$(TOPDIR)/feeds/luci/luci.mk|g' package/luci-app-msd_lite/Makefile
+
+# 修复 containerd 兼容 Go 1.22+ 编译报错（通配所有 2.x 版本）
+if [ -d "feeds/packages/utils/containerd" ]; then
+    find feeds/packages/utils/containerd/ -name "go.mod" -exec sed -i -E 's/github\.com\/klauspost\/cpuid\/v2 v[0-9\.]+/github.com\/klauspost\/cpuid\/v2 v2.2.3/g' {} +
+fi
