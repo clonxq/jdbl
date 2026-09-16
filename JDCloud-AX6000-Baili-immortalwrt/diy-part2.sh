917da02e8ce0f5ccb930e9844fc12b9d6c25e879
xqrock <58764635+xqrock@users.noreply.github.com>
2026-09-17 07:52:48 +0800

    Update diy-part2.sh

--- JDCloud-AX6000-Baili-immortalwrt/diy-part2.sh
+++ JDCloud-AX6000-Baili-immortalwrt/diy-part2.sh
@@ -30,10 +30,3 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 27.x feeds/packages/lang/golang
 #修复Rust编译失败
 sed -i 's/ci-llvm=true/ci-llvm=false/g' feeds/packages/lang/rust/Makefile
-#sed -i 's/mt7981b.dtsi/mt7981.dtsi/g' target/linux/mediatek/dts/*.dts*
-# 修复 containerd 兼容 Go 1.22+ 编译报错
-
-# 修复 containerd 兼容 Go 1.22+ 编译报错（通配所有 2.x 版本）
-if [ -d "feeds/packages/utils/containerd" ]; then
-    find feeds/packages/utils/containerd/ -name "go.mod" -exec sed -i -E 's/github\.com\/klauspost\/cpuid\/v2 v[0-9\.]+/github.com\/klauspost\/cpuid\/v2 v2.2.3/g' {} +
-fi
