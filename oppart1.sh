012f554eece6eb6f1e24aeaf388fff2498003820
xqrock <58764635+xqrock@users.noreply.github.com>
2026-09-21 22:57:01 +0800

    Update oppart1.sh

--- oppart1.sh
+++ oppart1.sh
@@ -57,6 +57,8 @@ function extract_pkg() {
 # extract_pkg "https://github.com/immortalwrt/luci" "applications/luci-app-eqos" "package/luci-app-eqos" "openwrt-25.12"
 
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone https://github.com/eamonxg/luci-theme-aurora.git package/luci-theme-aurora
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/passwall-packages
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall.git package/passwall-luci
 git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/OpenWrt-nikki
